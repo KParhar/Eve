77ca54c1e09f6c737b4d3fdd5f4d0e11ff40d150
KParhar <coderk2001@gmail.com>
2018-09-20 20:34:36 -0400

    2018-09-20

---
+++ app/src/main/java/com/dankov/eve/ServiceSelector.java
@@ -0,0 +1,45 @@
+package com.dankov.eve;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ServiceSelector extends Fragment {
+
+    public static final int NUMBER_SERVICES = 3;
+    Button services[] = new Button[NUMBER_SERVICES];
+
+    public ServiceSelector() {
+        //initApps((LinearLayout) (getActivity().findViewById(R.id.service_selector_layout)));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+
+        return inflater.inflate(R.layout.fragment_service_selector, container, false);
+    }
+
+    public void initApps(LinearLayout layout) {
+        for(int i = 0; i < NUMBER_SERVICES; i++) {
+            services[i] = new Button(getActivity());
+            services[i].setText("App " + i);
+
+            //layout.addView(services[i]);
+        }
+    }
+
+}
